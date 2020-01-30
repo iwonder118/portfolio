1a54e227434792e3a46465b85af66aa98ca4fd89
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2020-01-30 22:17:58 +0900

    Create-header

--- src/components/header.js
+++ src/components/header.js
@@ -2,33 +2,26 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+const Header = ({ }) => (
+  <header>
+    <nav className="navbar is-transparent is-dark is-fixed-top is-hidden-touch"
+      style={{ backgroundColor: "rgba(0,0,0,0.5)" }}>
+      <div className="navbar-menu container">
+        <div className="navbar-start">
+          <Link className="navbar-item has-text-weight-medium is-size-5 navbar-brand"
+            to="/" style={{ paddingRight: "40px" }}>
+            Wonder-Puffin
+          </Link>
+          <Link className="navbar-item is-size-6" to="/about">
+            About
+          </Link>
+          <Link className="navbar-item is-size-6" to="/works">
+            Works
+          </Link>
+        </div>
+      </div>
+    </nav>
+  </header >
 )
 
 Header.propTypes = {
